8838c8e8ecf1ff0154f71212e1bb7edfc88f3642
Henrick <ricknogueira1231@gmail.com>
2025-03-14 19:23:04 -0300

    #D117 13 - ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from "react-router-dom";
 import "./global.css";
 import { router } from "./routes";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <>
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from "lucide-react";
 import { Separator } from "./ui/separator";
-import { Link } from "react-router-dom";
 import { NavLink } from "./nav-link";
+import { ThemeToggle } from "./theme/theme-toggle";
+import { AccountMenu } from "./account-menu";
 
 export function Header() {
   return (
@@ -21,6 +22,12 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
+
       </div>
     </div>
   );
