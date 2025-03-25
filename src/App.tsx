1cf3e5c15571ebfe25bc450b6adb802311089f87
Henrick <ricknogueira1231@gmail.com>
2025-03-25 15:31:12 -0300

    #D128 24 - Configurando API Client

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import "./global.css";
 import { router } from "./routes";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
