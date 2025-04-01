d43fcbd7c77dfd9aaffc75fd7dcfd13091ea8065
Henrick <ricknogueira1231@gmail.com>
2025-03-31 21:01:49 -0300

    #D139 35 - Paginação dos pedidos

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,16 +1,23 @@
-import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
 import { Button } from "./ui/button";
 
 export interface PaginationProps {
   pageIndex: number;
   totalCount: number;
   perPage: number;
+  onPageChange: (pageIndex: number) => Promise<void> | void;
 }
 
 export function Pagination({
   pageIndex,
   totalCount,
   perPage,
+  onPageChange,
 }: PaginationProps) {
   const pages = Math.ceil(totalCount / perPage) || 1;
 
@@ -21,25 +28,45 @@ export function Pagination({
       </span>
       <div className="flex items-center gap-6 lg:gap-8">
         <div className="text-sm font-medium">
-            Página {pageIndex + 1} de {pages}
+          Página {pageIndex + 1} de {pages}
         </div>
         <div className="flex items-center gap-2">
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronsLeft className="h-4 w-4"/>
-                <span className="sr-only">Primeira página</span>
-            </Button>
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronLeft className="h-4 w-4"/>
-                <span className="sr-only">Página anterior</span>
-            </Button>
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronRight className="h-4 w-4"/>
-                <span className="sr-only">Próxima página</span>
-            </Button>
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronsRight className="h-4 w-4"/>
-                <span className="sr-only">Última página</span>
-            </Button>
+          <Button
+            onClick={() => onPageChange(0)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === 0}
+          >
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira página</span>
+          </Button>
+          <Button
+            onClick={() => onPageChange(pageIndex - 1)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === 0}
+          >
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Página anterior</span>
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => onPageChange(pageIndex + 1)}
+            disabled={pageIndex >= pages - 1}
+          >
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima página</span>
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => onPageChange(pages - 1)}
+            disabled={pageIndex >= pages - 1}
+          >
+            <ChevronsRight className="h-4 w-4" />
+            <span className="sr-only">Última página</span>
+          </Button>
         </div>
       </div>
     </div>
