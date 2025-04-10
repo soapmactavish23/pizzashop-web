16ab1fc29ceb1c428b9c13cd68a3c8d5ce27b163
Henrick <ricknogueira1231@gmail.com>
2025-04-10 20:11:04 -0300

    #D153 48 - Setup do ambiente de testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, InlineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    enviroment: 'happy-dom'
+  }
+} as UserConfig & {
+  test: InlineConfig
 });
